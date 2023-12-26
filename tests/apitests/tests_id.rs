4438b3b61b03186e2dca605f237f578f5d81ef9b
stiartsly <stiartsly@gmail.com>
2023-12-26 20:47:26 +0800

    Update testcase 'test_distance'

--- tests/apitests/tests_id.rs
+++ tests/apitests/tests_id.rs
@@ -16,12 +16,14 @@ mod apitests {
         let hex_id = Id::of_hex(hex_str).expect("Invalid hex Id");
         assert_eq!(hex_id.to_hex(), hex_str);
     }
+
     #[test]
     fn test_distance() {
         let a = Id::of_hex("00000000f528d6132c15787ed16f09b08a4e7de7e2c5d3838974711032cb7076").expect("Invalid hex Id");
         let b = Id::of_hex("00000000f0a8d6132c15787ed16f09b08a4e7de7e2c5d3838974711032cb7076").expect("Invalid hex Id");
         let distance_str = "0000000005800000000000000000000000000000000000000000000000000000";
         assert_eq!(a.distance(&b).to_hex(), distance_str);
+        assert_eq!(Id::distance(&a, &b).to_hex(), distance_str);
     }
 
     #[test]
