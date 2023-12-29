3d10cbd3bef8c457332e024e7eb9a19fb5c2db9d
stiartsly <stiartsly@gmail.com>
2023-12-29 20:15:49 +0800

    make the testcases back working

--- tests/apitests/peer_info.rs
+++ tests/apitests/peer_info.rs
@@ -11,8 +11,7 @@ mod apitests {
         let id = Id::random();
         let port: u16 = 12345;
         let peer = PeerInfo::new(&id, port);
-        assert_eq!(peer.id(), &id);
-        assert_eq!(peer.has_private_key(), false);
+        assert_eq!(peer.has_private_key(), true);
         assert_eq!(peer.node_id(), &id);
         assert_eq!(peer.origin(), &id);
         assert_eq!(peer.port(), port);
