148f03895fac476e9e75905ac9372d33b2c15eee
stiartsly <stiartsly@gmail.com>
2024-03-25 16:49:15 +0800

    WIP: update

--- apps/shell/main.rs
+++ apps/shell/main.rs
@@ -30,12 +30,12 @@ fn main() {
 
     let mut b = default_configuration::Builder::new();
     b.with_listening_port(32222);
-    //b1.with_ipv4("192.168.1.109");
-    b.with_ipv4("172.20.10.2");
+    b.with_ipv4("192.168.1.107");
+    //b.with_ipv4("172.20.10.2");
     b.with_storage_path(path.as_str());
 
     let id = Id::try_from_base58("6o6LkHgLyD5sYyW9iN5LNRYnUoX29jiYauQ5cDjhCpWQ").unwrap();
-    let addr = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(155, 138,245, 211)), 39001);
+    let addr = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(192, 168, 1, 107)), 39001);
     let node = NodeInfo::new(&id, &addr);
     b.add_bootstrap(&node);
 
