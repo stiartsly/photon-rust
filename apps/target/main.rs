d83ebc8a5b2a7a8fb781640c8c6ea3a81816fbab
stiartsly <stiartsly@gmail.com>
2024-05-15 09:38:15 +0800

    WIP: update configuration

--- apps/target/main.rs
+++ apps/target/main.rs
@@ -2,12 +2,9 @@ use std::env;
 use std::fs;
 use std::thread;
 use std::time::Duration;
-use std::net::{IpAddr, Ipv4Addr, SocketAddr};
 
 use boson::{
     default_configuration,
-    id::Id,
-    NodeInfo,
     node::Node
 };
 
@@ -26,23 +23,18 @@ fn get_storage_path(input: &str) -> String {
 }
 
 fn main() {
-    let path = get_storage_path(".shell_data");
+    let path = get_storage_path(".target_data");
 
     let mut b = default_configuration::Builder::new();
-    b.with_listening_port(32222);
+    b.with_listening_port(39001);
     b.with_ipv4("192.168.1.107");
     b.with_storage_path(path.as_str());
-
-    let id = Id::try_from_base58("6o6LkHgLyD5sYyW9iN5LNRYnUoX29jiYauQ5cDjhCpWQ").unwrap();
-    let addr = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(192, 168, 1, 107)), 39001);
-    let node = NodeInfo::new(&id, &addr);
-    b.add_bootstrap(&node);
 
     let cfg = b.build().unwrap();
 
     let mut runner = Node::new(cfg).unwrap();
     let _ = runner.start();
 
-    thread::sleep(Duration::from_secs(10));
+    thread::sleep(Duration::from_secs(100));
     runner.stop();
 }
